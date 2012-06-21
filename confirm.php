95eb8c26cdeb3f7448cb26305edd90d9567e8523
unknown <Yuhao@Yuhao-HP.(none)>
2012-06-21 01:06:28 -0400

    confirm.php looks pretty now, and works

--- confirm.php
+++ confirm.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+	$user_not_found = false;
+	$user_already_registered = true;
+	
 	include_once('private/php_scripts/dbObject.php');
 	$db = new dbObject;
 	$db->connect();
@@ -6,12 +9,56 @@
 	$get_id = $_GET['id'];
 	$user = $db->getUserByConfirmationCode($get_id);
 	if(!$user)
-		echo 'User Not Found';
+		$user_not_found = true;
 	else if($user->is_confirmed){
-		echo 'User Already Registered';
+		$user_already_registered = true;
 	}else{
 		$uid = $user->uid;
 		$db->confirmUser($uid);
-		echo 'User Registered';
 	}
 ?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Resumake | Online Resumes</title>
+	<link rel="stylesheet" type="text/css" href="private/bootstrap/css/bootstrap.css"></link>
+	<link rel="stylesheet" type="text/css" href="private/bootstrap/css/bootstrap-responsive.css"></link>
+</head>
+<body>
+	<br>
+	<div class="row">
+		<div class="page-header">
+			<h1 class="offset1">
+			<img src="private/imgs/logo.png"></img>
+			<small>Here's an idea, let's give everyone an online resume, for free.</small>
+			</h1>
+		</div>
+	</div>
+	<br>
+	
+	
+	<div class="row">
+		<div class="span10 offset2">
+		<div class="hero-unit">
+		<?php 
+			if($user_not_found)
+				echo "<h1>We Can't Seem To Find Your Account</h1><h2>Sorry</h2>";
+			else if($user_already_registered)
+				echo "<h1>You Are Already Registered!</h1><h2>You Can't Re-Register</h2>";
+			else if(!$user_not_found && !$user_already_registered)
+				echo '<h1>Congratulations ' . $user->name . '!</h1><h2>You Are Now Registered!</h2>';
+			?>
+		</div>
+		</div>
+	</div>
+	
+	<?php 
+		if(!$user_not_found)
+			echo '<div class="row"><div class="span8 offset2"><h2>You\'ll be redirected to your homepage in a few seconds</h2></div></div><br><div class="row"><div class="span8 offset2"><h2>Or Go Now </h2><a class="btn btn-primary btn-large" href="/' . $user->username . '">Go To Your Homepage</a></div></div>';
+		else echo '<div class="row"><div class="span8 offset2"><h2>Go Back To The Homepage</h2><a class="btn btn-primary btn-large" href="/">Go Home</a></div></div>';
+	?>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="private/bootstrap/js/bootstrap.js"></script>
+</body>
