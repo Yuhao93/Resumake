fc5636fe860d06ab85b41c1f15e2dd1aac82ab8b
The GingerBread Club <thegbclub@wezen.dreamhost.com>
2012-07-04 19:50:50 -0700

    continue image upload

--- private/default/index.php
+++ private/default/index.php
@@ -4,6 +4,7 @@
 
 <?php
 	$uid;
+	$fileUploaded = false;
 	session_start();
 	if(!isset($_COOKIE['remember']) && !isset($_SESSION['uid'])){
 		header('Location: /');
@@ -18,6 +19,15 @@
 	if($user)		
 		$user_info = json_decode($user->info);
 	else $user_info = json_decode('{}');
+
+        if(sizeof($_FILES) != 0){
+		$md5uid = md5($uid);
+		$name = $_FILES['img']['name'];
+		$tmp = $_FILES['img']['tmp_name'];
+		$newpath = 'imgs/' . $md5uid . '-' . $name;
+		move_uploaded_file($tmp,$newpath);
+		$fileUploaded = true; 
+	}
 ?>
 
 <!DOCTYPE HTML>
@@ -52,169 +62,171 @@
     </div>
 
     <div class="container-fluid">
-      <div class="row-fluid">
-        <div class="span3 columns">
-			<div class="page-header">
-				<h1><?php echo $user->name;?></h1>
-			</div>
-			
-			<div class="thumbnail">
-				<img src="private/default/me.jpg" width="254" height="254" alt="">
-				<br>
-				<br>
-				<center>
-					<a class="btn btn-primary" href="#picture-modal" data-toggle="modal">Change Profile Picture</a>
-				</center>
-				<br>
-			</div>
+	<div class="row-fluid">
+	<div class="span3 columns">
+		<div class="page-header">
+			<h1><?php echo $user->name;?></h1>
+		</div>
+		
+		<div class="thumbnail">
+			<img src="private/default/me.jpg" width="254" height="254" alt="">
 			<br>
-			<div class="well">
-				<p><h4>Personal Information</h4></p>
-				<p><h5 id='info-age'>&nbsp;&nbsp;Age: <?php if($user_info)echo $user_info->{'age'}?></h5></p>
-				<p><h5 id='info-birthday'>&nbsp;&nbsp;Birthday: <?php if($user_info)echo $user_info->{'birthday'}?></h5></p>
-				<p><h5 id='info-gender'>&nbsp;&nbsp;Gender: <?php if($user_info)echo $user_info->{'gender'}?></h5></p>
-				<p><h5 id='info-occupation'>&nbsp;&nbsp;Occupation: <?php if($user_info)echo $user_info->{'occupation'}?></h5></p>
-				<br>
+			<br>
+			<center>
+				<a class="btn btn-primary" href="#picture-modal" data-toggle="modal">Change Profile Picture</a>
+			</center>
+			<br>
+		</div>
+		<br>
+		<div class="well">
+			<p><h4>Personal Information</h4></p>
+			<p><h5 id='info-age'>&nbsp;&nbsp;Age: <?php if($user_info)echo $user_info->{'age'}?></h5></p>
+			<p><h5 id='info-birthday'>&nbsp;&nbsp;Birthday: <?php if($user_info)echo $user_info->{'birthday'}?></h5></p>
+			<p><h5 id='info-gender'>&nbsp;&nbsp;Gender: <?php if($user_info)echo $user_info->{'gender'}?></h5></p>
+			<p><h5 id='info-occupation'>&nbsp;&nbsp;Occupation: <?php if($user_info)echo $user_info->{'occupation'}?></h5></p>
+			<br>
+			
 				
+			<p><h4>Contact Information</h4></p>
+			<p><h5 id='info-address'>&nbsp;&nbsp;Address: <?php if($user_info)echo $user_info->{'address'}?></h5></p>
+			<p><h5 id='info-city'>&nbsp;&nbsp;City: <?php if($user_info)echo $user_info->{'city'}?></h5></p>
+			<p><h5 id='info-state'>&nbsp;&nbsp;State: <?php if($user_info)echo $user_info->{'state'}?></h5></p>
+			<p><h5 id='info-zip'>&nbsp;&nbsp;Zip Code: <?php if($user_info)echo $user_info->{'zip'}?></h5></p>
+			<p><h5>&nbsp;&nbsp;Email: <a id='info-email' href="mailto:<?php if($user_info)echo $user_info->{'email'} ?>"><?php if($user_info)echo $user_info->{'email'}?></a></h5></p>
+			<p><h5 id='info-phone'>&nbsp;&nbsp;Phone Number: <?php if($user_info)echo $user_info->{'phone'}?></h5></p>
+			<br>
+			<a class="btn btn-primary btn-large" href="#infomodal" id="editinfo" data-toggle="modal">Edit My Information</a>
 				
-				<p><h4>Contact Information</h4></p>
-				<p><h5 id='info-address'>&nbsp;&nbsp;Address: <?php if($user_info)echo $user_info->{'address'}?></h5></p>
-				<p><h5 id='info-city'>&nbsp;&nbsp;City: <?php if($user_info)echo $user_info->{'city'}?></h5></p>
-				<p><h5 id='info-state'>&nbsp;&nbsp;State: <?php if($user_info)echo $user_info->{'state'}?></h5></p>
-				<p><h5 id='info-zip'>&nbsp;&nbsp;Zip Code: <?php if($user_info)echo $user_info->{'zip'}?></h5></p>
-				<p><h5>&nbsp;&nbsp;Email: <a id='info-email' href="mailto:<?php if($user_info)echo $user_info->{'email'} ?>"><?php if($user_info)echo $user_info->{'email'}?></a></h5></p>
-				<p><h5 id='info-phone'>&nbsp;&nbsp;Phone Number: <?php if($user_info)echo $user_info->{'phone'}?></h5></p>
-				<br>
-				<a class="btn btn-primary btn-large" href="#infomodal" id="editinfo" data-toggle="modal">Edit My Information</a>
-				
-				<br>
-			</div>
+			<br>
+		</div>
         </div>	
         <div class="span9 fixed-inbox">
-			<div class="modal hide" id="picture-modal">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">x</button>
-					<h3>Upload Your Profile Picture</h3>
-				</div>
-				<div class="modal-body">
-					<form class="well" action="private/php_scripts/image_upload.php" method="post">
-						<input type="file" accept="image/*" id="image-upload" name="img"/>
-						<input type="hidden" value="<?php echo $uid?>"/>
-					</form>
-				</div>
-				<div class="modal-footer">
-					<a href="#" class="btn" data-dismiss="modal">Cancel</a>
-					<a href="#" class="btn btn-primary" data-dismiss="modal" id="picture-continue">Continue</a>
-				</div>
-				
-			</div> 
-			<div class="modal hide" id="infomodal">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">×</button>
-					<h3>Edit Your Information</h3>
-				</div>
-				<div class="modal-body">
-					<form class="well">
-						<h2>Personal Information</h2>
-						<label>Age</label>
-						<input type="text" class="span5" id="modal-age">
-						<label>Birthday</label>
-						<input type="date" class="span5" id="modal-birthday">
-						<label>Gender</label>
-						<select class="span5" id="modal-gender">
-							<option>Male</option>
-							<option>Female</option>
-						</select>
-						<label>Occupation</label>
-						<input type="text" class="span5" id="modal-occupation">
-						
-						<h2>Contact Information</h2>
-						<label>Address</label>
-						<input type="text" class="span5" id="modal-address">
-						<label>City</label>
-						<input type="text" class="span5" id="modal-city">
-						<label>State</label>
-						<select class="span5" id="modal-state">
-							<option></option>
-							<option>Alabama</option>
-							<option>Alaska</option>
-							<option>Arizona</option>
-							<option>Arkansas</option>
-							<option>California</option>
-							<option>Colorado</option>
-							<option>Connecticut</option>
-							<option>Delaware</option>
-							<option>Florida</option>
-							<option>Georgia</option>
-							<option>Hawaii</option>
-							<option>Idaho</option>
-							<option>Illinois</option>
-							<option>Indiana</option>
-							<option>Iowa</option>
-							<option>Kansas</option>
-							<option>Kentucky</option>
-							<option>Louisiana</option>
-							<option>Maine</option>
-							<option>Maryland</option>
-							<option>Massachusetts</option>
-							<option>Michigan</option>
-							<option>Minnesota</option>
-							<option>Mississippi</option>
-							<option>Missouri</option>
-							<option>Montana</option>
-							<option>Nebraska</option>
-							<option>Nevada</option>
-							<option>New Hampshire</option>
-							<option>New Jersey</option>
-							<option>New Mexico</option>
-							<option>New York</option>
-							<option>North Carolina</option>
-							<option>North Dakota</option>
-							<option>Ohio</option>
-							<option>Oklahoma</option>
-							<option>Oregon</option>
-							<option>Pennsylvania</option>
-							<option>Rhode Island</option>
-							<option>South Carolina</option>
-							<option>South Dakota</option>
-							<option>Tennessee</option>
-							<option>Texas</option>
-							<option>Utah</option>
-							<option>Vermont</option>
-							<option>Virginia</option>
-							<option>Washington</option>
-							<option>West Virginia</option>
-							<option>Wisconsin</option>
-							<option>Wyoming</option>
-						</select>
-						<label>Zip Code</label>
-						<input type="text" class="span5" id="modal-zipcode">
-						<label>Email</label>
-						<input type="text" class="span5" id="modal-email">
-						<label>Phone Number</label>
-						<input type="text" class="span5" id="modal-phonenumber">
-					
-					</form>
-				</div>
-				<div class="modal-footer">
-					<a href="#" class="btn" data-dismiss="modal">Cancel</a>
-					<a href="#" class="btn btn-primary" id="confirm-info" data-dismiss="modal">Save</a>
-				</div>
+		<div class="modal hide" id="picture-modal">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">x</button>
+				<h3>Upload Your Profile Picture</h3>
+			</div>
+			<div class="modal-body">
+				<form class="well" enctype="multipart/form-data" action="<?php echo $user?>.php" method="post">
+					<input type="file" name="img"/>
+					<input type="hidden" name="uid" value="<?php echo $uid?>"/>
+					<br>
+					<input type="submit" name="submit" value="submit" class="btn btn-primary"/>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<a href="#" class="btn" data-dismiss="modal">Cancel</a>
+				<!--<a href="#" class="btn btn-primary" data-dismiss="modal" id="picture-continue">Continue</a>-->
 			</div>
 			
-			<h3>My Resumes</h3>
-			<a href="#" class="btn-primary btn btn-large">Add New Resume</a>
-			<table class="table table-striped">
-				<thead>
-					<th>Resume</th>
-					<th>For</th>
-					<th>Created On</th>
-					<th>Description</th>
-				</thead>
-			</table>
+		</div> 
+		<div class="modal hide" id="infomodal">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">×</button>
+				<h3>Edit Your Information</h3>
+			</div>
+			<div class="modal-body">
+				<form class="well">
+					<h2>Personal Information</h2>
+					<label>Age</label>
+					<input type="text" class="span5" id="modal-age">
+					<label>Birthday</label>
+					<input type="date" class="span5" id="modal-birthday">
+					<label>Gender</label>
+					<select class="span5" id="modal-gender">
+						<option>Male</option>
+						<option>Female</option>
+					</select>
+					<label>Occupation</label>
+					<input type="text" class="span5" id="modal-occupation">
+					
+					<h2>Contact Information</h2>
+					<label>Address</label>
+					<input type="text" class="span5" id="modal-address">
+					<label>City</label>
+					<input type="text" class="span5" id="modal-city">
+					<label>State</label>
+					<select class="span5" id="modal-state">
+						<option></option>
+						<option>Alabama</option>
+						<option>Alaska</option>
+						<option>Arizona</option>
+						<option>Arkansas</option>
+						<option>California</option>
+						<option>Colorado</option>
+						<option>Connecticut</option>
+						<option>Delaware</option>
+						<option>Florida</option>
+						<option>Georgia</option>
+						<option>Hawaii</option>
+						<option>Idaho</option>
+						<option>Illinois</option>
+						<option>Indiana</option>
+						<option>Iowa</option>
+						<option>Kansas</option>
+						<option>Kentucky</option>
+						<option>Louisiana</option>
+						<option>Maine</option>
+						<option>Maryland</option>
+						<option>Massachusetts</option>
+						<option>Michigan</option>
+						<option>Minnesota</option>
+						<option>Mississippi</option>
+						<option>Missouri</option>
+						<option>Montana</option>
+						<option>Nebraska</option>
+						<option>Nevada</option>
+						<option>New Hampshire</option>
+						<option>New Jersey</option>
+						<option>New Mexico</option>
+						<option>New York</option>
+						<option>North Carolina</option>
+						<option>North Dakota</option>
+						<option>Ohio</option>
+						<option>Oklahoma</option>
+						<option>Oregon</option>
+						<option>Pennsylvania</option>
+						<option>Rhode Island</option>
+						<option>South Carolina</option>
+						<option>South Dakota</option>
+						<option>Tennessee</option>
+						<option>Texas</option>
+						<option>Utah</option>
+						<option>Vermont</option>
+						<option>Virginia</option>
+						<option>Washington</option>
+						<option>West Virginia</option>
+						<option>Wisconsin</option>
+						<option>Wyoming</option>
+					</select>
+					<label>Zip Code</label>
+					<input type="text" class="span5" id="modal-zipcode">
+					<label>Email</label>
+					<input type="text" class="span5" id="modal-email">
+					<label>Phone Number</label>
+					<input type="text" class="span5" id="modal-phonenumber">
+				
+				</form>
+			</div>
+			<div class="modal-footer">
+				<a href="#" class="btn" data-dismiss="modal">Cancel</a>
+				<a href="#" class="btn btn-primary" id="confirm-info" data-dismiss="modal">Save</a>
+			</div>
 		</div>
+			
+		<h3>My Resumes</h3>
+		<a href="#" class="btn-primary btn btn-large">Add New Resume</a>
+		<table class="table table-striped">
+			<thead>
+				<th>Resume</th>
+				<th>For</th>
+				<th>Created On</th>
+				<th>Description</th>
+			</thead>
+		</table>
 		</div>
 	</div>
+</div>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="private/bootstrap/js/bootstrap.js"></script>
