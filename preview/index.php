0cb1d8d99691280bb182ca5dcbb29267b347ccba
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-14 12:24:57 -0700

    A

--- preview/index.php
+++ preview/index.php
@@ -10,12 +10,71 @@
     <div class="hero-unit row span12">
         <h1>Resumake: The Online Resume Solution</h1>
         <br><br>
-        <h2>Sign Up Your Email For More Info</h2>
-        <div class="row span12">
-            <input type="text" style="height:32px;width:750px;font-size:24px;padding-top:13px;padding-bottom:13px;margin-top:8px;" placeholder="Your Email">
-            <a href="#" class="btn btn-primary btn-large" style="display:inline-block;width:250px;height:36px;border-radius:2px;font-size:24px;">Sign Up</a>
+        <h2>Sign With Your Email For More Info</h2>
+        <div class="row span10" style="position:relative;">
+            <input type="text" style="height:32px;width:100%;font-size:24px;padding-top:13px;padding-bottom:10px;margin-top:8px;" placeholder="Your Email">
+            <a href="#" class="btn btn-primary btn-large" style="display:inline-block;width:190px;height:36px;border-radius:2px;font-size:24px;position:absolute;right:-10px;top:8px;">Sign Up</a>
         </div>
     </div>
+    
+    
+    <div class="row span12">
+        <br><br><br><br><br><br>
+        <center><h1 class="row span12">Create An Online Resume That You Can Easily Show Your Future Employers</h1></center>
+        <br><br><br><br><br>
+        <section class="span5">
+            <div class="page-header">
+                <h2>Resumake Lets You Have An Online Resume For Free.<br><small>No Webserver or Programming Needed.</small></h2>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat massa a quam ultricies sagittis. Sed vitae ultrices turpis. Cras sodales, elit at auctor ullamcorper, massa lorem varius nunc, a convallis velit nisl a leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat fringilla lobortis. Aenean condimentum dapibus odio. Nullam eu imperdiet lorem. Phasellus rhoncus diam ut sem molestie viverra ut in diam. Sed sed libero quis erat scelerisque vulputate. Nunc semper dui quis arcu adipiscing ornare. Cras adipiscing, metus at dictum condimentum, velit elit viverra mi, sit amet pharetra urna tellus a est. Sed fermentum pellentesque orci, nec dapibus urna sodales eget.</p>
+        </section>
+        <div class="thumbnail span6">
+            <img src="../private/imgs/home1.png">
+        </div>
+    </div>
+    <div class="row span12">
+        <br><br><br><br><br><br>
+        <center><h1 class="row span12">No Tricky Web Programming, No Hassle, Just That Simple</h1></center>
+        <br><br><br><br><br>
+        <div class="thumbnail span5">
+            <img src="../private/imgs/home2.png">
+        </div>
+        <section class="span6">
+            <div class="page-header">
+                <h2>Upload A Resume And You're Done<br><small>No Need For Any Web Programming Or Rented Server Space</small></h2>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat massa a quam ultricies sagittis. Sed vitae ultrices turpis. Cras sodales, elit at auctor ullamcorper, massa lorem varius nunc, a convallis velit nisl a leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat fringilla lobortis. Aenean condimentum dapibus odio. Nullam eu imperdiet lorem. Phasellus rhoncus diam ut sem molestie viverra ut in diam. Sed sed libero quis erat scelerisque vulputate. Nunc semper dui quis arcu adipiscing ornare. Cras adipiscing, metus at dictum condimentum, velit elit viverra mi, sit amet pharetra urna tellus a est. Sed fermentum pellentesque orci, nec dapibus urna sodales eget.</p>
+        </section>
+    </div>
+    
+    <div class="row span12">
+        <br><br><br><br><br><br>
+        <center><h1 class="row span12">Let Resumake Play Match-maker</h1></center>
+        <br><br><br><br><br>
+        <section class="span5">
+            <div class="page-header">
+                <h2>Employers Are Looking For Resumes And You Have One<br><small>We Think We Can Work Something Out.</small></h2>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat massa a quam ultricies sagittis. Sed vitae ultrices turpis. Cras sodales, elit at auctor ullamcorper, massa lorem varius nunc, a convallis velit nisl a leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat fringilla lobortis. Aenean condimentum dapibus odio. Nullam eu imperdiet lorem. Phasellus rhoncus diam ut sem molestie viverra ut in diam. Sed sed libero quis erat scelerisque vulputate. Nunc semper dui quis arcu adipiscing ornare. Cras adipiscing, metus at dictum condimentum, velit elit viverra mi, sit amet pharetra urna tellus a est. Sed fermentum pellentesque orci, nec dapibus urna sodales eget.</p>
+        </section>
+        <div class="thumbnail span6">
+            <img src="../private/imgs/home3.png">
+        </div>
+    </div>
+    
+    <div class="row span12">
+        <br><br><br><br><br><br>
+        <section class="span12">
+            <div class="page-header">
+                <h1>Coming Soon<br><small>Sign Up Now</small></h1>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean feugiat massa a quam ultricies sagittis. Sed vitae ultrices turpis. Cras sodales, elit at auctor ullamcorper, massa lorem varius nunc, a convallis velit nisl a leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat fringilla lobortis. Aenean condimentum dapibus odio. Nullam eu imperdiet lorem. Phasellus rhoncus diam ut sem molestie viverra ut in diam. Sed sed libero quis erat scelerisque vulputate. Nunc semper dui quis arcu adipiscing ornare. Cras adipiscing, metus at dictum condimentum, velit elit viverra mi, sit amet pharetra urna tellus a est. Sed fermentum pellentesque orci, nec dapibus urna sodales eget.</p>
+        </section>
+    </div>
+    <div class="row span12">
+    <br><br><br><br><br><br><br>
+    </div>
+    
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="../private/bootstrap/js/bootstrap.js"></script>
 </body>
