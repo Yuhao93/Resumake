96bdd1235bec341bc7a7e96595cc1b7c322ce31c
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-14 03:24:17 -0700

    Added a preview page

---
+++ preview/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Resumake | It's Coming</title>
+	<link rel="stylesheet" type="text/css" href="../private/bootstrap/css/bootstrap.css"></link>
+	<link rel="stylesheet" type="text/css" href="../private/bootstrap/css/bootstrap-responsive.css"></link>
+</head>
+<body>
+    <br>
+    <div class="hero-unit row span12">
+        <h1>Resumake: The Online Resume Solution</h1>
+        <br><br>
+        <h2>Sign Up Your Email For More Info</h2>
+        <div class="row span12">
+            <input type="text" style="height:32px;width:750px;font-size:24px;padding-top:13px;padding-bottom:13px;margin-top:8px;" placeholder="Your Email">
+            <a href="#" class="btn btn-primary btn-large" style="display:inline-block;width:250px;height:36px;border-radius:2px;font-size:24px;">Sign Up</a>
+        </div>
+    </div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="../private/bootstrap/js/bootstrap.js"></script>
+</body>
+</head>
