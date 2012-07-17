e4b4a93b8e1b79bdbca16243c09d822adff9a81e
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-16 19:02:48 -0700

    A

--- preview/index.php
+++ preview/index.php
@@ -36,8 +36,8 @@
         <h2>Sign Up With Your .edu Email For More Info</h2>
         <div id="alert-container"></div>
         <div class="row span10" style="position:relative;margin-left:0px;"> 
-            <input type="text" style="height:32px;width:78%;font-size:24px;padding-top:13px;padding-bottom:10px;margin-top:8px;" placeholder="Your Email" id="preview-email">
-            <a href="#" class="btn btn-primary btn-large" style="display:inline-block;width:20%;height:36px;border-radius:2px;font-size:24px;position:absolute;right:-10px;top:8px;" id="preview-register">Sign Up</a>
+            <input type="text" class="preview-email" style="height:32px;width:78%;font-size:24px;padding-top:13px;padding-bottom:10px;margin-top:8px;" placeholder="Your Email">
+            <a href="#" class="btn btn-primary btn-large preview-register" style="display:inline-block;width:20%;height:36px;border-radius:2px;font-size:24px;position:absolute;right:-10px;top:8px;">Sign Up</a>
         </div>
         <div class="row span10" style="margin-left:0px;">
 
@@ -107,8 +107,8 @@
             <p>Give us your email and we'll let you know as soon as Resumake is up and running. With our first release coming soon, be sure to get in on the action right when its released.</p>
         </section>
         <div class="row span11" style="position:relative;margin-left:0px;"> 
-            <input type="text" style="height:32px;width:78%;font-size:24px;padding-top:13px;padding-bottom:10px;margin-top:8px;" placeholder="Your Email" id="preview-email">
-            <a href="#" class="btn btn-primary btn-large" style="display:inline-block;width:20%;height:36px;border-radius:2px;font-size:24px;position:absolute;right:-10px;top:8px;" id="preview-register">Sign Up</a>
+            <input type="text" style="height:32px;width:78%;font-size:24px;padding-top:13px;padding-bottom:10px;margin-top:8px;" placeholder="Your Email" class="preview-email">
+            <a href="#" class="btn btn-primary btn-large preview-register" style="display:inline-block;width:20%;height:36px;border-radius:2px;font-size:24px;position:absolute;right:-10px;top:8px;">Sign Up</a>
         </div>
     </div>
     <div class="row span11">
@@ -131,12 +131,12 @@
         }
     }
     $(document).ready(function(){
-        $("#preview-register").each(function(index){
+        $(".preview-register").each(function(index){
             $(this).click(function(){
             register();
             });
         });
-        $('#preview-register').each(function(index){
+        $('.preview-email').each(function(index){
             $(this).keypress(function(e){
                 if(e.which == 13){
                     register();
