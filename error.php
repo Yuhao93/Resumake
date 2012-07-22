e061b093f48e994d59617be07879f641626c0e76
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-21 23:23:02 -0700

    A

---
+++ error.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>The Error Page</title>
+	<link rel="stylesheet" type="text/css" href="private/bootstrap/css/bootstrap.css"></link>
+	<link rel="stylesheet" type="text/css" href="private/bootstrap/css/bootstrap-responsive.css"></link>
+</head>
+<body>
+    <div class="container">
+        <div class="hero-unit">
+            <h1>Oops!</h1>
+            <h2>We couldn't find the webpage you were looking for.</h2>
+        </div>
+    </div>
+    <?php include "private/php_scripts/encryption.php" ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="private/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-33395111-1']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</body>
--- users.php
+++ users.php
@@ -608,3 +608,4 @@
         })();
     </script>
 </body>
+</html>
