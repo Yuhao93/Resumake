e4de5450ff91b0d5e5ed8ebd3c558b641a1ec223
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-21 22:28:10 -0700

    A

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
 	}
 ?>
 
-<!DOCTYPE HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
 <head>
 	<title>Resumake | Online Resumes</title>
