f116106dd69e967cb28417c30cc4b97006ba2f96
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-21 20:04:45 -0700

    A

--- users.php
+++ users.php
@@ -423,6 +423,7 @@
         });
         
         $.post('../private/php_scripts/resumeedit.php', {'request':'delete', 'resumes':resumeDelete, 'username':username, 'uid':uid}, function(data){
+            alert(data);
             var content = "";
             if(data != "]"){
                 var resumes = eval('(' + data + ')');
