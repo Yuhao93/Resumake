c6283ae850e50768fe20f5f4af1f43927923f31e
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-22 18:32:09 -0700

    A

--- linkedin.php
+++ linkedin.php
@@ -15,6 +15,10 @@
 		padding-top:60px;
 	}
 	</style>
+    <script type="text/javascript" src="http://platform.linkedin.com/in.js">
+        api_key: zb8gl61hj3lq
+        onLoad: onLinkedInLoad
+    </script>
 </head>
 <body>
     <div class="navbar navbar-fixed-top">
@@ -31,68 +35,31 @@
 			</div>
 		</div>
     </div>
-    
-    <p>This example demonstrates the use of a login button to control what's displayed.  It also demonstrates how to use the LinkedIn auth events in a user flow.</p>
 
 
-<script type="text/javascript" src="http://platform.linkedin.com/in.js">
-  api_key: zb8gl61hj3lq
-  onLoad: onLinkedInLoad
-
-</script>
-
-
-<p>This example demonstrates the use of a login button to control what's displayed.  It also demonstrates how to use the LinkedIn auth events in a user flow.</p>
-
-<!-- NOTE: be sure to set onLoad: onLinkedInLoad -->
-<script type="text/javascript">
-function onLinkedInLoad() {
-  IN.Event.on(IN, "auth", function() {onLinkedInLogin();});
-  IN.Event.on(IN, "logout", function() {onLinkedInLogout();});
-}
-
-function onLinkedInLogout() {
-  setLoginBadge(false);
-}
-
-function onLinkedInLogin() {
-  // we pass field selectors as a single parameter (array of strings)
-  IN.API.Profile("me")
-    .fields(["id", "firstName", "lastName", "pictureUrl", "publicProfileUrl"])
-    .result(function(result) {
-      setLoginBadge(result.values[0]);
-    })
-    .error(function(err) {
-      alert(err);
-    });
-}
-
-function setLoginBadge(profile) {
-  if (!profile) {
-    profHTML = "<p>You are not logged in</p>";
-  }
-  else {
-    var pictureUrl = profile.pictureUrl || "http://static02.linkedin.com/scds/common/u/img/icon/icon_no_photo_80x80.png";
-    profHTML = "<p><a href=\"" + profile.publicProfileUrl + "\">";
-    profHTML = profHTML + "<img align=\"baseline\" src=\"" + pictureUrl + "\"></a>";      
-    profHTML = profHTML + "&nbsp; Welcome <a href=\"" + profile.publicProfileUrl + "\">";
-    profHTML = profHTML + profile.firstName + " " + profile.lastName + "</a>! <a href=\"#\" onclick=\"IN.User.logout(); return false;\">logout</a></p>";
-  }
-  document.getElementById("loginbadge").innerHTML = profHTML;
-}
-</script>
-
-<!-- need to be logged in to use; if not, offer a login button -->
-<script type="IN/Login"></script>
-
-<div id="loginbadge">
-  <p>Login badge renders here if the current user is authorized.</p>
-</div>
+    <script type="IN/Login"></script>
     
     
     
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="private/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript">
+        function onLinkedInLoad() {
+            IN.Event.on(IN, "auth", function() {onLinkedInLogin();});
+        }
+        function onLinkedInLogin() {
+            var requestedAttributes = ["id", "firstName", "lastName", "pictureUrl", "publicProfileUrl"];
+            IN.API.Profile("me")
+                .fields(requestedAttributes)
+                .result(function(result) {
+                    importLinkedInProfile(result.values[0]);
+                })
+                .error(function(err) {
+                });
+        }
+        function importLinkedInProfile(profile) {
+        }
+    </script>
     <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-33395111-1']);
