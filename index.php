cc0c08be0d474f2267ab7f92cd0fe99b5de0a400
unknown <Yuhao@Yuhao-HP.(none)>
2012-07-29 21:49:15 -0700

    A

--- index.php
+++ index.php
@@ -130,6 +130,55 @@
 		</div>
 		
 	</div>
+    
+    <div class="row span11">
+        <br>
+        <center><h1 class="row span11">Create An Online Resume That You Can Easily Show Your Future Employers</h1></center>
+        <br><br><br><br><br>
+        <section class="span5">
+            <div class="page-header">
+                <h2>Resumake Lets You Have An Online Resume For Free.<br><small>No Webserver or Programming Needed.</small></h2>
+            </div>
+            <p>Adapt to the changing times and get an online resume. Today, companies not only accept online resumes but also prefer them. It makes it easier for them to find certain skills and experiences in their applicants. Needless to say, having an online resume is becoming a necessity.</p>
+            <p>Let Resumake give you an online resume. Free and Simple.</p>
+            <p>After you have uploaded your online resume, companies that are eager to find new employees will be able to access our database of resumes. Our double-blinded match-making process ensures that when a company contacts you, both sides will be excited to pursue the offer.</p>
+        </section>
+        <div class="thumbnail span5">
+            <img src="/private/imgs/home1.png">
+        </div>
+    </div>
+    <div class="row span11">
+        <br><br><br><br><br><br>
+        <center><h1 class="row span11">No Tricky Web Programming, No Hassle, Just That Simple</h1></center>
+        <br><br><br><br><br>
+        <div class="thumbnail span5">
+            <img src="/private/imgs/home2.png">
+        </div>
+        <section class="span5">
+            <div class="page-header">
+                <h2>Upload A Resume And You're Done<br><small>No Need For Any Web Programming Or Rented Server Space</small></h2>
+            </div>
+            <p>Typically, the process of making an online resume is long and expensive. Creating a professional looking website is hard and time-consuming. In order to do it well, you have to invest a lot of time that could be spent doing other things.</p>
+            <p>To host up your webpage, you need server space. This can be achieved through a free website making service that creates an unprofessional and unnattractive website or through renting or buying server space, which can often times be expensive.</p>
+            <p>Resumake makes it easy to host an online resume. We don't charge you any fee for hosting your resume and we streamline your resume making process. You can even import your profile from LinkedIn to make the process even easier</p>
+        </section>
+    </div>
+    
+    <div class="row span11">
+        <br><br><br><br><br><br>
+        <center><h1 class="row span11">Let Resumake Play Match-maker</h1></center>
+        <br><br><br><br><br>
+        <section class="span5">
+            <div class="page-header">
+                <h2>Employers Are Looking For Resumes And You Have One<br><small>We Think We Can Work Something Out.</small></h2>
+            </div>
+            <p>Resumake's advanced match-making algorithm makes both parties benefit. Every applicant that a company pursues that doesn't end in an acceptance of an offer is time and money lost. Likewise, any company that you pursue that doesn't end in an offer is time lost.</p>
+            <p>Our double-blind match-making algorithm makes sure that companies target the resumes that are the best fit for their criteria and that you only show up on the radars of companies that can make an offer to appease both sides.</p>
+        </section>
+        <div class="thumbnail span5">
+            <img src="/private/imgs/home3.png">
+        </div>
+    </div>
   </div>
 	<?php include "private/php_scripts/encryption.php" ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
